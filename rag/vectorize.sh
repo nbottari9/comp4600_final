16dfa882f37f016482d1329dcc102f782858b2b2
ComposedNebula <81245965+ComposedNebula@users.noreply.github.com>
2025-11-11 12:35:01 -0500

    finish the scripts

--- rag/vectorize.sh
+++ rag/vectorize.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 html_files=$(ls ./html/*.html)
 echo $html_files
-
-ramalama --engine podman rag $html_files ./output/
+mkdir output
+# everyone else
+ramalama rag $html_files ./output/
+# FOR ME AND NICO
+# ramalama --engine podman rag $html_files ./output/
